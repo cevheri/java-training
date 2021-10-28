4d5d5efc2e7e4f7d98cbe98f9c577c084b0ba8b8
sibel <sibel_kukey@hotmail.com>
2021-10-28 15:24:00 +0300

    anasayfa değiştirildi.

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -9,15 +9,12 @@ import setupAxiosInterceptors from './config/axios-interceptor';
 import { clearAuthentication } from './shared/reducers/authentication';
 import ErrorBoundary from './shared/error/error-boundary';
 import AppComponent from './app';
-import { loadIcons } from './config/icon-loader';
 
 const store = getStore();
 registerLocale(store);
 
 const actions = bindActionCreators({ clearAuthentication }, store.dispatch);
 setupAxiosInterceptors(() => actions.clearAuthentication('login.error.unauthorized'));
-
-loadIcons();
 
 const rootEl = document.getElementById('root');
 
